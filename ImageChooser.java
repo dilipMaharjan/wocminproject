2067ce25a3d4f5242cb8cd50aecf911a8b92a4ee
Nathan  Albers <nathanalbers@Nathans-MacBook-Pro.local>
2017-06-14 15:39:39 -0400

    UI with comments

--- ImageChooser.java
+++ ImageChooser.java
@@ -4,7 +4,10 @@ import java.awt.*;
 import java.io.*;
 import javax.swing.filechooser.*;
 
-
+//	Main Frame, includes
+// 	> Image Select Button
+//  > Text Field to display file path
+//	> Upload Button
 public class ImageChooser extends JFrame {
 	
 	private JButton uploadButton, imageButton;
@@ -13,26 +16,21 @@ public class ImageChooser extends JFrame {
 	
 	public ImageChooser() {
 		
-		
-		setTitle("Image Chooser");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(800, 250);
-		setLocation(300, 300);
-		
 		uploadButton = new JButton("Upload");
-		uploadButton.setEnabled(false);
+		uploadButton.setEnabled(false); // Disables upload button until a readable image is selected
+		
 		imageButton = new JButton("Choose an image");
 		
 		field = new JTextField(25);
-		field.setEditable(false);
+		field.setEditable(false); // Ensures that users will only attempt to upload viable images
 		
+		// Create and add an ActionListener to buttons
 		FileChooseListener f1 = new FileChooseListener();
-		
 		imageButton.addActionListener(f1);
 		uploadButton.addActionListener(f1);
 		
-		panel1 = new JPanel();
-		panel2 = new JPanel();
+		panel1 = new JPanel(); // image button and text field
+		panel2 = new JPanel(); // upload button
 				
 		panel1.add(imageButton, BorderLayout.NORTH);
 		panel1.add(field, BorderLayout.CENTER);
@@ -41,11 +39,17 @@ public class ImageChooser extends JFrame {
 		add(panel1, BorderLayout.NORTH);
 		add(panel2, BorderLayout.CENTER);
 		
+		// Window specifications
+		setTitle("Image Chooser");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 250);
+		setLocation(300, 300);
 		setLocation(300, 300);
 		setVisible(true);
 		
 	}
 	
+	// Error Window class that displays a pop up message to help correct the problem
 	public class ErrorWindow extends JFrame {
 		JPanel panel = new JPanel();
 		
@@ -69,6 +73,7 @@ public class ImageChooser extends JFrame {
 		}
 	}
 	
+	// Action Listener for both image select and upload buttons
 	public class FileChooseListener implements ActionListener {
 		File file = new File("");
 		ImageIcon image = new ImageIcon();
@@ -83,21 +88,23 @@ public class ImageChooser extends JFrame {
 				if (selectedFile == JFileChooser.APPROVE_OPTION) {
 					file = newChooser.getSelectedFile();
 					
+					// Ensure file is an image in the correct format
 					if (file.getPath().endsWith(".jpg") || file.getPath().endsWith(".png")) {
-			
 						image = new ImageIcon(file.getPath());
 						field.setText(file.getPath());
 						field.setEditable(true);
 						uploadButton.setEnabled(true);
 					}
 					else {
-						new ErrorWindow(1);
+						new ErrorWindow(1); 
 						return;
 					}
 				}
 				
 			}
 			
+			// Temporarily, displays the image in a new window
+			// will eventually upload image to the server
 			if (e.getSource() == uploadButton) {
 				
 					JFrame newFrame = new JFrame();
