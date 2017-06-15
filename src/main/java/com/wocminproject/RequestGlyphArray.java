464a5f8620db4c0f22b171f41e8b6c9a4ef50279
dmaharjan <dilipmaharjann@gmail.com>
2017-06-15 10:09:16 -0400

    Created post request method that gets the json array from server of the provided image and dumpbs the data in the console

---
+++ src/main/java/com/wocminproject/RequestGlyphArray.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wocminproject;
+
+import java.io.File;
+import java.net.URLConnection;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ *
+ * @author dmaharjan
+ */
+public class RequestGlyphArray {
+
+    public static void main(String[] args) {
+        File file = new File("test2.png");
+        new RequestGlyphArray().getGlyphInfo(file);
+    }
+
+    private void getGlyphInfo(File file) {
+        try {
+
+            String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+            System.out.println("mime type: " + mimeType);
+            MediaType imageType = MediaType.parse(mimeType);
+
+            OkHttpClient client = new OkHttpClient();
+
+            RequestBody requestBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("image", "test2.png",
+                            RequestBody.create(imageType, file))
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url("http://172.19.144.219:12345/images")
+                    .post(requestBody)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            String json = response.body().string();
+            System.out.println("I am the response" +json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
