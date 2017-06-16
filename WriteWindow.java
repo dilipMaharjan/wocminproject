2ed2b80e5cc64855c7f0198ddcc6c7077a35f592
Nathan  Albers <nathanalbers@Nathans-MacBook-Pro.local>
2017-06-16 10:03:46 -0400

    Save Window

--- WriteWindow.java
+++ WriteWindow.java
@@ -7,10 +7,11 @@ import java.util.*;
 
 public class WriteWindow extends JFrame {
 
-	private JButton fileButton;
+	private JButton fileButton, selectButton;
+	private JTextField field;
 	private JPanel panel1, panel2;
 	private JLabel label;
-	private String fileToWrite;
+	private String fileToWrite, location;
 	
 	// Call new WriteWindow with an argument of the string to be written to the file
 	public WriteWindow(String file) {
@@ -20,19 +21,30 @@ public class WriteWindow extends JFrame {
 		panel1 = new JPanel();
 		panel2 = new JPanel();
 	
-		fileButton = new JButton("Save As");
-		label = new JLabel("Choose a Location for your file");
+		fileButton = new JButton("Save");
+		label = new JLabel("          Choose a Location for your file");
+		
+		selectButton = new JButton("Choose Location");
+		selectButton.addActionListener(new ButtonListener());
+		
+		field = new JTextField(30);
+		field.setEditable(false);
+
 		
 		fileButton.addActionListener(new ButtonListener());
 		
-		panel1.add(label, BorderLayout.NORTH);
-		panel2.add(fileButton);
+		panel1.setLayout(new BorderLayout());
 		
+		panel1.add(label, BorderLayout.WEST);
+		panel1.add(selectButton, BorderLayout.EAST);
+		panel1.add(field, BorderLayout.SOUTH);
+		
+		panel2.add(fileButton, BorderLayout.SOUTH);
 		
 		add(panel1, BorderLayout.NORTH);
-		add(panel2, BorderLayout.CENTER);
+		add(panel2, BorderLayout.SOUTH);
 		
-		setSize(500, 150);
+		setSize(400, 150);
 		setLocation(400, 300);
 		setResizable(false);
 		setTitle("Save Window");
@@ -44,29 +56,30 @@ public class WriteWindow extends JFrame {
 		
 		public void actionPerformed(ActionEvent e) {
 
+			if(e.getSource() == selectButton) {
 				JFileChooser newChooser = new JFileChooser();
 				newChooser.setCurrentDirectory(null);
 				
 				int saveFile = newChooser.showSaveDialog(null);
 				if(saveFile == JFileChooser.APPROVE_OPTION) {
 					
-					try {
-						String location = newChooser.getSelectedFile().getPath().trim();
+						location = newChooser.getSelectedFile().getPath().trim();
 						if(!location.endsWith(".txt")){
 							location += ".txt";
 						}
-						 
-						FileWriter newWriter = new FileWriter(location);
-						PrintWriter pWriter = new PrintWriter(newWriter);
-						
-						pWriter.println(fileToWrite);
-						pWriter.close();
-						
-					} catch (IOException err) {
-						}
 				}
-			
+				field.setText(location);
+			}
+				
+			if (e.getSource() == fileButton) {
+				try {
+					FileWriter newWriter = new FileWriter(location);
+					PrintWriter pWriter = new PrintWriter(newWriter);
+					
+					pWriter.println(fileToWrite);
+					pWriter.close();
+				} catch (IOException error1) {}
+			} 
 		}
 	}
-	
 }
